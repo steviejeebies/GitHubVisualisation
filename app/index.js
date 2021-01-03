84e4841b7e1dcaad842847c3bea916df6c7b2091
Stephen Rowe <rowes@tcd.ie>
2021-01-03 12:11:59 +0000

    recreated drawing the graphs on submission of username (from yesod project)

--- app/index.js
+++ app/index.js
@@ -2,5 +2,11 @@ drawHistogram({a:"b"})
 
 drawPieChart({a:"b"})
 
+function preventDefault(event) { 
+    event.preventDefault(); 
+    drawBarChart({a:"b"})
+} 
+document.getElementById("getthingy").addEventListener('submit', preventDefault);
+
 
 
