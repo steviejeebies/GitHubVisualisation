6893e28c9a046c44f5c29c4552c34142164bb68c
Stephen Rowe <rowes@tcd.ie>
2021-01-05 00:51:38 +0000

    created a specific object from a API call that we can use to pass to multiline graph

--- app/index.js
+++ app/index.js
@@ -2,6 +2,10 @@ drawHistogram({a:"b"})
 
 // https://github.com/search -- test your results against this\
 
+var outResult
+var repoNumCommits
+let mLParameter
+
 const header = {
     "Accept": "application/vnd.github.v3+json",
     "Authorization" : `Token ${AUTHTOKEN}`
@@ -12,8 +16,6 @@ const header_commit = {
     "Accept": "application/vnd.github.cloak-preview+json, application/vnd.github.v3+json",
     "Authorization" : `Token ${AUTHTOKEN}`
 }
-
-drawMultiLineGraph(1);
 
 const url = "https://api.github.com/"
 
@@ -55,9 +57,46 @@ async function usernameInput(event) {
     today.setFullYear(today.getFullYear() - 1 );
     let lastYear = today.toISOString().slice(0, 10);
 
-    let callURL = `search/commits?q=user:${username} author-date:>${lastYear}`
+    let callURL = `search/commits?q=author:${username} author-date:>${lastYear}`
 
     result = await paginationAllOneArray(url.concat(callURL), {"method" : "GET", "headers": header_commit})
+
+    // Now that we have all the commits, I want to nest them according to repository. This can easily be done 
+    // with a D3 function
+
+    var nestedReposFull = d3v3.nest()
+    .key(function(d) { return d.repository.full_name; })
+    .entries(result);
+
+    // The issue is that we have to count the total of commits for each day, and there doesn't seem to 
+    // be a clean way to do this other than a for-loop, again relying on D3's nesting function. Once 
+    // we have the nesting done, we can create elements to pass to multiLineGraph.js by constructing an 
+    // object of the form {count: Number, month: DateString, name:RepoNameString}
+    repoNumCommits = []
+
+    for(let i = 0; i < nestedReposFull.length; i++)
+    {
+        repoNumCommits.push(d3v3.nest()
+                                .key(function(d) { return d.commit.author.date.substring(0, 10); })
+                                .entries(nestedReposFull[i].values))
+    }
+
+    mLParameter = []
+    repoNumCommits.forEach(s => {
+        s.forEach(t => {
+            mLParameter.push({count: t.values.length, month: t.key, name: t.values[0].repository.full_name})
+        })
+    })
+
+    // Now we have the exact structure we need for the multiLineGraph
+
+    drawMultiLineGraph(mLParameter);
+
+    // nestedReposFull.items.forEach(i => {
+    //     repoNumCommits.push(d3v3.nest()
+    //                             .key(function(d) { return d.values.commit.date.substring(0, 9); })
+    //                             .entries(outResult))
+    // })
 
     // let commitInfoForMultiLine = []
     // result.forEach(    
@@ -123,7 +162,8 @@ async function getPrivateRepos() {
 
 async function paginationAllOneArray (passURL, headerMethodObject) {
     let nextURL = passURL.concat("&per_page=150")
-    let returnArray = []
+    let returnArray = [];
+    let urlElement = undefined
 
     do {
         let responce = await fetch(nextURL, headerMethodObject)
@@ -131,16 +171,18 @@ async function paginationAllOneArray (passURL, headerMethodObject) {
         if(result.items === undefined) break;
         returnArray.push(...result.items)
         let link = responce.headers.get("link")
-        let links = link.split(",")
-        urls = links.map(a=> {
-            return {
-                url: a.split(";")[0].replace(">","").replace("<",""),
-                title:a.split(";")[1]
-            }
-        })
-        urlElement = urls.find(item => {
-            return item.title === ' rel="next"'
-         })
+        if (link !== null) {
+            let links = link.split(",")
+            urls = links.map(a=> {
+                return {
+                    url: a.split(";")[0].replace(">","").replace("<",""),
+                    title:a.split(";")[1]
+                }
+            })
+            urlElement = urls.find(item => {
+                return item.title === ' rel="next"'
+            })
+        }
         if(urlElement) nextURL = urlElement.url
     } while(urlElement)
     return returnArray
